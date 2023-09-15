1a27f2edfc7b4669aeb6e7f0ae285a15ebd060e7
Tafsirul islam Rohan <mdrohan8432@gmail.com>
2023-09-15 23:46:27 +0600

    added editing file

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,26 +1,31 @@
 
-const Course = (course) => {
-    console.log(course.course)
-    const { ImageURL, name, description, price, credit } = course.course;
-    return (
-   <div>
-         <div className="w-64 p-3 bg-white rounded-md h-[350px]">
-            <div>
-                <img src={ImageURL} alt="" className="mb-3" />
-            </div>
-            <h3 className="text-center mb-3">{name}</h3>
-            <p className="mb-3"><small>{description}</small></p>
-            <div className="flex justify-evenly mb-3">
-                <p>{price}</p>
-                <p>{credit}</p>
-            </div>
-            <div className="text-center mb-3" >
-            <button className="bg-blue-400 h-10 w-52 rounded-md text-white font-bold">Select</button>
-            </div>
-        </div>
+// const Course = (course) => {
 
-   </div>
-    );
-};
+   
+//     const { ImageURL, name, description, price, credit } = course.course;
+//     return (
+//    <div>
+//          <div className="w-64 p-3 bg-white rounded-md h-[350px]">
+//             <div>
+//                 <img src={ImageURL} alt="" className="mb-3" />
+//             </div>
+//             <h3 className="text-center mb-3">{name}</h3>
+//             <p className="mb-3"><small>{description}</small></p>
+//             <div className="flex justify-evenly mb-3">
+//                 <p>{price}</p>
+//                 <p>{credit}</p>
+//             </div>
+//             <div className="text-center mb-3" >
+//             <button className="bg-blue-400 h-10 w-52 rounded-md text-white font-bold">Select</button>
+//             </div>
+//         </div>
 
-export default Course;
+//    </div>
+//     );
+// };
+
+// <Course key={courses.id} course={course}></Course>
+
+// const { ImageURL, name, description, price, credit } = course.course;
+
+// export default Course;
--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Course from "./course";
+
 
 const Courses = () => {
     const [courses, setCourses] = useState([])
@@ -10,16 +10,37 @@ const Courses = () => {
             .then(res => res.json())
             .then(data => setCourses(data));
     }, [])
-
+    console.log(courses)
+   
     return (
 
         <div className="flex mx-20">
 
             <div className="grid grid-cols-3 gap-6">
                 {
-                    courses.map(course => <Course key={courses.id} course={course}></Course>)
-                }
-
+                    courses.map(course =>(
+                       
+                        
+                       <div key={course.id}>
+                             <div className="w-64 p-3 bg-white rounded-md h-[350px]">
+                                <div>
+                                    <img src={course.ImageURL} alt="" className="mb-3" />
+                                </div>
+                                <h3 className="text-center mb-3">{course.name}</h3>
+                                <p className="mb-3"><small>{course.description}</small></p>
+                                <div className="flex justify-evenly mb-3">
+                                    <p>{course.price}</p>
+                                    <p>{course.credit}</p>
+                                </div>
+                                <div className="text-center mb-3" >
+                                <button className="bg-blue-400 h-10 w-52 rounded-md text-white font-bold">Select</button>
+                                </div>
+                            </div>
+                    
+                       </div>
+                    ) )
+                
+                    }
             </div>
 
             <div>
